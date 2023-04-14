3a208f9c9555f82a9fb979bb8c656d8dcb4d0bdf
mukeshrsvteksystems.com <mukesh.r@svteksystems.com>
2023-04-14 07:07:44 +0530

    FunctionComponent

--- src/Table.js
+++ src/Table.js
@@ -1,34 +1,48 @@
 import { Component } from "react";
 
+const TableHeader = () => {
+    return(
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+        </tr>
+    </thead>
+    )
+   
+}
+
+const TableBody = () => {
+   return(
+    <tbody>
+    <tr>
+        <td>Charlie</td>
+        <td>FE developer</td>
+    </tr>
+    <tr>
+        <td>David</td>
+        <td>BE developer</td>
+    </tr>
+    <tr>
+        <td>Watson</td>
+        <td>PM</td>
+    </tr>
+    <tr>
+        <td>Bravo</td>
+        <td>HR</td>
+    </tr>
+</tbody>
+   )
+}
+
 
 class Table extends Component{
     render(){
         return(
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Charlie</td>
-                        <td>FE developer</td>
-                    </tr>
-                    <tr>
-                        <td>David</td>
-                        <td>BE developer</td>
-                    </tr>
-                    <tr>
-                        <td>Watson</td>
-                        <td>PM</td>
-                    </tr>
-                    <tr>
-                        <td>Bravo</td>
-                        <td>HR</td>
-                    </tr>
-                </tbody>
+              <TableHeader/>
+              <TableBody/>
+               
             </table>
         )
     }
